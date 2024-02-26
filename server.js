8cdcb6f3b8afd10f7e1d6fd30d849e7c61bcc13b
teasing <jlovela@naver.com>
2024-02-26 15:04:55 +0900

    backend final

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
 let tweets = [
   {
-    id: '1',
-    text: 'first one',
-    userId: '2',
+    id: "1",
+    text: "first one!",
+    userId: "2",
   },
   {
-    id: '2',
-    text: 'secend on',
-    userId: '1',
+    id: "2",
+    text: "second one",
+    userId: "1",
   },
 ];
 
 let users = [
   {
-    id: '1',
-    firstName: 'teasin',
-    lastName: 'cho',
+    id: "1",
+    firstName: "nico",
+    lastName: "las",
   },
   {
-    id: '2',
-    firstName: 'Elon',
-    lastName: 'mask',
+    id: "2",
+    firstName: "Elon",
+    lastName: "Mask",
   },
 ];
 
@@ -31,58 +32,110 @@ const typeDefs = gql`
     id: ID!
     firstName: String!
     lastName: String!
-    # fullname is the sum of firstName + lastName as a string
+    """
+    Is the sum of firstName + lastName as a string
+    """
     fullName: String!
   }
-  #Tweet object represents a resource for a tweet
-
+  """
+  Tweet object represents a resource for  a Tweet
+  """
   type Tweet {
     id: ID!
     text: String!
     author: User
   }
-
   type Query {
+    allMovies: [Movie!]!
     allUsers: [User!]!
     allTweets: [Tweet!]!
-    tweet(id: ID!): Tweet!
+    tweet(id: ID!): Tweet
+    movie(id: String!): Movie
   }
-
   type Mutation {
-    postTweet(text: String!, userId: ID!): Tweet
-    #Deletes a Tweet if found, else return false
+    postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Deletes a Tweet if found, else returns false
+    """
     deleteTweet(id: ID!): Boolean!
   }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
+  }
 `;
+// GET /api/v1/tweets
+// POST DELETE PUT /api/v1/tweets
+// GET /api/v1/tweet/:id
 
 const resolvers = {
   Query: {
-    allTweets: () => tweets,
-    tweet: (root, { id }) => tweets.find((tweet) => tweet.id === id),
-    allUsers: () => users,
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+    allUsers() {
+      console.log("allUsers called!");
+      return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((r) => r.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((r) => r.json())
+        .then((json) => json.data.movie);
+    },
   },
   Mutation: {
-    postTweet: (root, { text, userId }) => {
+    postTweet(_, { text, userId }) {
       const newTweet = {
-        id: (tweets.length + 1).toString(),
+        id: tweets.length + 1,
         text,
         userId,
       };
       tweets.push(newTweet);
       return newTweet;
     },
-    deleteTweet: (root, { id }) => {
-      const tweetIndex = tweets.findIndex((tweet) => tweet.id === id);
-      if (tweetIndex === -1) return false;
-      tweets.splice(tweetIndex, 1);
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
       return true;
     },
   },
   User: {
-    fullName: (user) => `${user.firstName} ${user.lastName}`,
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
   },
   Tweet: {
-    author: ({ userId }) => users.find((user) => user.id === userId),
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
+    },
   },
 };
 
